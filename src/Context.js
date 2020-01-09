ecec7e78235515b33801f3aa43ba2e0cc9400fd2
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-01-09 13:15:06 +0800

    Make remove day functionality

--- src/Context.js
+++ src/Context.js
@@ -7,22 +7,35 @@ const initialState = {
   items: []
 }
 
-const day = {
-  Header: '',
-  key: '',
-}
-
 export const Provider = (props) => {
   const [state, updateState ] = useState(initialState);
   const [days, updateDays] = useState(0)
+
+  const refreshItems = (items) => {
+    items.forEach((item, i) => {
+      const day = i + 1
+      item.Header = `Day ${day}`
+      item.key = day
+      console.log(day)
+    });
+  }
+
   const actions = {
     addNewDay() {
       console.log("Add new day passed to context")
-      const items = state.items;
+      const { items } = state;
       const currentDay = days + 1
       updateDays(currentDay);
       items.push({Header: `Day ${currentDay}`, key: currentDay})
       updateState({...state, items })
+    },
+    removeDay(key) {
+      console.log(`Remove day ${key}`);
+      const { items } = state;
+      const filteredItems = items.filter((item) => item.key !== key);
+      updateDays(filteredItems.length)
+      refreshItems(filteredItems) // Refresh items
+      updateState({...state, items:filteredItems}) // remove
     }
   };
   const { children } = props
