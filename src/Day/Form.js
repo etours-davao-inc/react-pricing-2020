3db707d8c9de156133b54033a8c19e1c8ed471dd
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-01-10 15:17:14 +0800

    Fix f(x) of Form Select and Checkboxes

--- src/Day/Form.js
+++ src/Day/Form.js
@@ -8,7 +8,10 @@ export default ({ k }) => {
   const [state, setState] = useState(initialState);
   const onChange = (e) => {
     console.log(e.target.name, e.target.value)
-    setState({ ...state, [e.target.name]: e.target.value })
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    setState({ ...state, [name]: value })
   }
 
   const onSubmit = (e) => {
@@ -18,31 +21,58 @@ export default ({ k }) => {
   return (
     <form className="form-inline" onSubmit={onSubmit}>
       <input type="text" name="item" placeholder="Item" className="form-control mr-1" onChange={onChange} />
-      <select id="type" className="custom-select mr-1" onChange={onChange} >
+      <select className="custom-select mr-1" name="type" onChange={onChange} value={state.type} >
         <option value="misc">Miscellaneous</option>
-        <option value="" defaultValue>Type</option>
         <option value="accommodation">Accommodation</option>
         <option value="transportation">Transportation</option>
         <option value="meals">Meals</option>
         <option value="tourguide">Tourist guide fee</option>
         <option value="entrancefee">Entrance fee</option>
       </select>
-      <input type="number" step="any" name="price" placeholder="0.0" className="form-control mr-2 text-right" onChange={onChange}  />
+      <input 
+        type="number" 
+        step="any" 
+        name="price" 
+        placeholder="0.0" 
+        className="form-control mr-2 text-right" 
+        onChange={onChange} 
+      />
       <div className="form-group mr-2">
         <div className="form-check">
-          <input type="checkbox" id="shared" name="shared" className="form-check-input" onChange={onChange}  />
+          <input 
+            type="checkbox" 
+            id="shared" 
+            name="shared" 
+            className="form-check-input" 
+            checked={state.shared} 
+            onChange={onChange} 
+          />
           <label htmlFor="shared" className="form-check-label">Shared</label>
         </div>
       </div>
       <div className="form-group mr-2">
         <div className="form-check">
-          <input type="checkbox" id="vat" name="vat" className="form-check-input" onChange={onChange} />
+          <input 
+            type="checkbox" 
+            id="vat" 
+            name="vat" 
+            className="form-check-input" 
+            checked={state.vat} 
+            onChange={onChange} 
+          />
           <label htmlFor="vat" className="form-check-label">Vat</label>
         </div>
       </div>
       <div className="form-group mr-2">
         <div className="form-check">
-          <input type="checkbox" id="coh" name="coh" className="form-check-input" onChange={onChange} />
+          <input 
+            type="checkbox" 
+            id="coh" 
+            name="coh" 
+            className="form-check-input" 
+            checked={state.coh} 
+            onChange={onChange} 
+          />
           <label htmlFor="coh" className="form-check-label">CoH</label>
         </div>
       </div>
