439889c8d6a06c35aecb1b41a8a0f9257112ae47
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-01-13 15:37:40 +0800

    HandleSubmit, set uniique id's, reset to initialState

--- src/Day/Form.js
+++ src/Day/Form.js
@@ -3,7 +3,7 @@ import { PricingContext } from '../Context';
 
 
 export default ({ k }) => {
-  const initialState = { key: k, item: "", type: "", price: "", shared: false, vat: false, coh: false, remarks: "" }
+  const initialState = {key: k, item: "", type: "miscellaneous", price: "", shared: false, vat: false, coh: false, remarks: "" };
   const { priceItemSubmit } = useContext(PricingContext);
   const [state, setState] = useState(initialState);
   const onChange = (e) => {
@@ -16,64 +16,68 @@ export default ({ k }) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    priceItemSubmit(state)
+    priceItemSubmit(state);
+    setState(initialState)
   }
+  
   return (
     <form className="form-inline" onSubmit={onSubmit}>
-      <input type="text" name="item" placeholder="Item" className="form-control mr-1" onChange={onChange} />
+      <input type="text" name="item" placeholder="Item" className="form-control mr-1" onChange={onChange} value={state.item} />
       <select className="custom-select mr-1" name="type" onChange={onChange} value={state.type} >
-        <option value="misc">Miscellaneous</option>
+        <option value="miscellaneous" defaultValue>Miscellaneous</option>
         <option value="accommodation">Accommodation</option>
         <option value="transportation">Transportation</option>
         <option value="meals">Meals</option>
         <option value="tourguide">Tourist guide fee</option>
         <option value="entrancefee">Entrance fee</option>
       </select>
-      <input 
-        type="number" 
-        step="any" 
-        name="price" 
-        placeholder="0.0" 
-        className="form-control mr-2 text-right" 
-        onChange={onChange} 
+      <input
+        type="number"
+        step="any"
+        name="price"
+        placeholder="0.0"
+        className="form-control mr-2 text-right"
+        onChange={onChange}
+        value={state.price}
       />
       <div className="form-group mr-2">
         <div className="form-check">
-          <input 
-            type="checkbox" 
-            id="shared" 
-            name="shared" 
-            className="form-check-input" 
-            checked={state.shared} 
-            onChange={onChange} 
+          <input
+            type="checkbox"
+            id={`shared-${state.key}`}
+            name="shared"
+            className="form-check-input"
+            checked={state.shared}
+            onChange={onChange}
+
           />
-          <label htmlFor="shared" className="form-check-label">Shared</label>
+          <label htmlFor={`shared-${state.key}`} className="form-check-label">Shared</label>
         </div>
       </div>
       <div className="form-group mr-2">
         <div className="form-check">
-          <input 
-            type="checkbox" 
-            id="vat" 
-            name="vat" 
-            className="form-check-input" 
-            checked={state.vat} 
-            onChange={onChange} 
+          <input
+            type="checkbox"
+            id={`vat-${state.key}`}
+            name="vat"
+            className="form-check-input"
+            checked={state.vat}
+            onChange={onChange}
           />
-          <label htmlFor="vat" className="form-check-label">Vat</label>
+          <label htmlFor={`vat-${state.key}`} className="form-check-label">Vat</label>
         </div>
       </div>
       <div className="form-group mr-2">
         <div className="form-check">
-          <input 
-            type="checkbox" 
-            id="coh" 
-            name="coh" 
-            className="form-check-input" 
-            checked={state.coh} 
-            onChange={onChange} 
+          <input
+            type="checkbox"
+            id={`coh-${state.key}`}
+            name="coh"
+            className="form-check-input"
+            checked={state.coh}
+            onChange={onChange}
           />
-          <label htmlFor="coh" className="form-check-label">CoH</label>
+          <label htmlFor={`coh-${state.key}`} className="form-check-label">CoH</label>
         </div>
       </div>
       <input type="text" name="remarks" placeholder="remarks" className="form-control mr-1" onChange={onChange} />
