1181fc59f65fc64417d4a818272059d10422b9b8
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-01-28 10:08:49 +0800

    Add min, max, commission fields and handle submit

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,14 @@ import './App.css';
 import Items from './Items';
 
 const TaxMarkUpForm = () => {
-  const { markup, tax, updateTaxMarkUp } = useContext(PricingContext);
-  const [state, setState] = useState({markup, tax});
+  const { markup, tax, min, max, commission, updateTaxMarkUp } = useContext(PricingContext);
+  const [state, setState] = useState({ markup, tax, min, max, commission });
 
   const onChange = (e) => {
-    const newState = {...state, [e.target.name]: e.target.value}
+    const name = e.target.name;
+    let value = e.target.value;
+    if (['markup', 'tax', 'commission'].includes(name)) value = value/100;
+    const newState = { ...state, [name]: value }
     setState(newState);
     updateTaxMarkUp(newState);
   }
@@ -18,30 +21,72 @@ const TaxMarkUpForm = () => {
     <div className="form-inline mb-2">
       <div className="input-group">
         <div className="input-group-prepend">
-          <div className="input-group-text">Mark Up %</div>
+          <div className="input-group-text">Min</div>
         </div>
-        <input 
-          type="number" 
-          id="markup" 
-          name="markup" 
-          onChange={onChange} 
-          placeholder="0%" 
+        <input
+          type="number"
+          id="min"
+          name="min"
+          onChange={onChange}
+          placeholder="0"
           className="form-control mr-1 text-right font-weight-bold"
-          value={markup}
+          value={min}
         />
       </div>
       <div className="input-group">
         <div className="input-group-prepend">
-          <div className="input-group-text">Margin %</div>
+          <div className="input-group-text">Max</div>
         </div>
-        <input 
-          type="number" 
-          id="tax" 
-          name="tax" 
-          onChange={onChange} 
-          placeholder="0%" 
+        <input
+          type="number"
+          id="max"
+          name="max"
+          onChange={onChange}
+          placeholder="0"
           className="form-control mr-1 text-right font-weight-bold"
-          value={tax}
+          value={max}
+        />
+      </div>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <div className="input-group-text">Mark Up %</div>
+        </div>
+        <input
+          type="number"
+          id="markup"
+          name="markup"
+          onChange={onChange}
+          placeholder="0%"
+          className="form-control mr-1 text-right font-weight-bold"
+          value={`${(markup*100).toFixed(1)}`}
+        />
+      </div>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <div className="input-group-text">Tax %</div>
+        </div>
+        <input
+          type="number"
+          id="tax"
+          name="tax"
+          onChange={onChange}
+          placeholder="0%"
+          className="form-control mr-1 text-right font-weight-bold"
+          value={`${(tax*100).toFixed(1)}`}
+        />
+      </div>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <div className="input-group-text">Commission %</div>
+        </div>
+        <input
+          type="number"
+          id="commission"
+          name="commission"
+          onChange={onChange}
+          placeholder="0"
+          className="form-control mr-1 text-right font-weight-bold"
+          value={`${(commission*100).toFixed(1)}`}
         />
       </div>
     </div>
@@ -49,19 +94,47 @@ const TaxMarkUpForm = () => {
 }
 
 export default () => {
-  const { name, total } = useContext(PricingContext);
+  const { name } = useContext(PricingContext);
   return (
 
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello, {name}</h1>
-          <TaxMarkUpForm />
-          <Items />
-          <p>{JSON.stringify(total)}</p>
-        </header>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, {name}</h1>
+        <TaxMarkUpForm />
+        <Items />
+        <Table />
+      </header>
+    </div>
 
   );
+}
+
+export const Table = () => {
+  const { total } = useContext(PricingContext);
+  return (
+    <table>
+      <thead>
+        <tr>
+          {total.prices[0].map((elem, index) => {
+            return (
+              <td key={index}>{elem}</td>
+            )
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {total.prices.map((elem, index) => {
+          if (index > 0) {
+            return (
+              <tr key={index}>
+                {elem.map((e,i) => <td key={i}>{e}</td>)}
+              </tr>
+            )
+          }
+        })}
+      </tbody>
+    </table>
+  )
 }
 
 
