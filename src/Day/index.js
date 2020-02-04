2636839e6769e12d9b9440f023aa336c53966fa0
Tom Claudio <526473+gb96@users.noreply.github.com>
2020-02-04 16:43:23 +0800

    Implement editable rows and row for adding expense item

--- src/Day/index.js
+++ src/Day/index.js
@@ -1,10 +1,8 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useState } from 'react';
 import { PricingContext } from '../Context';
-import { FaTimesCircle, FaEdit } from 'react-icons/fa';
+import { FaTimesCircle, FaCheckCircle } from 'react-icons/fa';
 import './Day.css';
-import Form from './Form';
-
-
+import './Form.css';
 const CloseButton = ({ k }) => {
   const { removeDay } = useContext(PricingContext);
   return (
@@ -27,7 +25,6 @@ const ExpenseTable = ({ children }) => (
     <table>
       <thead>
         <tr>
-          <th>Id</th>
           <th>Item</th>
           <th>Type</th>
           <th>Price</th>
@@ -45,56 +42,195 @@ const ExpenseTable = ({ children }) => (
   </div>
 )
 
-const Expense = (props) => {
-  const { deleteItem } = useContext(PricingContext);
-  const { 
-    id,
-    day,
-    item,
-    type,
-    price,
-    remarks,
-    shared,
-    coh,
-    vat } = props.item;
-  const { onClick } = props;  
+const Expense = ({item}) => {
+  console.log('Context state', item)
+  const { deleteItem, updatePriceItem } = useContext(PricingContext);
+  const [ state, setState ] = useState(item)
+  console.log('internal state', state)
+  const onChange = (e) => {
+    // console.log(e.target.name, e.target.value)
+    const { name, type } = e.target;
+    let { value } = e.target;
+    value = type === 'checkbox' ? e.target.checked : value;
+    if (type === 'number') value = Number(value);
+    const newState = { ...state, [name]: value }
+    if (type === 'checkbox' || type === 'select') updatePriceItem(newState);
+    setState(newState)
+  }
+
+  const update = () => {
+    if (state.item === "" || state.price === 0 || state.price < 0) {
+      setState(item);
+      return
+    }
+    updatePriceItem(state)
+  }
+
   return (
     <tr>
-      <td>{id.slice(-4,-1)}</td>
-      <td>{item}</td>
-      <td>{type}</td>
-      <td>{`Php ${price}`}</td>
-      <td>{shared && 'Shared'}</td>
-      <td>{vat && 'Vat'}</td>
-      <td>{coh && 'CoH'}</td>
-      <td>{remarks}</td>
-      <td><FaEdit style={{ color: 'green', fontSize:'1.5em' }} onClick={() => onClick()} /></td>
-      <td><FaTimesCircle style={{ color: 'red', fontSize:'1.5em' }} onClick={() => deleteItem(id)} /></td>
+      <td><input type="text" name="item" value={state.item} className="form-input" onChange={onChange} onBlur={update} /></td>
+      <td>
+        <select className="custom-select mr-1" name="type" value={state.type} onChange={onChange}>
+          <option value="miscellaneous" defaultValue>Miscellaneous</option>
+          <option value="accommodation">Accommodation</option>
+          <option value="transportation">Transportation</option>
+          <option value="meals">Meals</option>
+          <option value="tourguide">Tourist guide fee</option>
+          <option value="entrance fee">Entrance fee</option>
+        </select>
+      </td>
+      <td><input type="number" name="price" className="text-right" value={state.price} onChange={onChange} onBlur={update} /></td>
+      <td>
+        <input
+          type="checkbox"
+          id='shared'
+          name="shared"
+          className="form-input"
+          checked={state.shared}
+          onChange={onChange}
+        />
+      </td>
+      <td>
+        <input
+          type="checkbox"
+          id='vat'
+          name="vat"
+          className="form-input"
+          checked={state.vat}
+          onChange={onChange}
+        />
+      </td>
+      <td>
+        <input
+          type="checkbox"
+          id='coh'
+          name="coh"
+          className="form-input"
+          checked={state.coh}
+          onChange={onChange}
+        />
+      </td>
+      <td>
+        <input 
+          type="text" 
+          name="remarks" p
+          laceholder="remarks" 
+          className="form-input" 
+          value={state.remarks} 
+          onChange={onChange} 
+          onBlur={update} 
+        />
+      </td>
+      <td><FaTimesCircle style={{ color: 'red', fontSize: '1.5em' }} onClick={() => deleteItem(state.id)} /></td>
     </tr>
   )
 };
 
-export default (props) => {
-  const formRef = useRef(null);
-  const { expenses } = useContext(PricingContext);
-  const { day } = props;
-  const todaysExpenses = expenses.filter(expense => expense.day === day)
+const NewExpense = ({day}) => {
+  const NewItem = {
+    id: null,
+    day: day,
+    item: "", 
+    type: "miscellaneous", 
+    price: "", 
+    shared: false, 
+    vat: false, 
+    coh: false, 
+    remarks: ""   
+  }
+  const { priceItemSubmit } = useContext(PricingContext);
+  const [ state, setState ] = useState(NewItem)
+  console.log('internal state', state)
+  const onChange = (e) => {
+    // console.log(e.target.name, e.target.value)
+    const { name, type } = e.target;
+    let { value } = e.target;
+    value = type === 'checkbox' ? e.target.checked : value;
+    if (type === 'number') value = Number(value);
+    const newState = { ...state, [name]: value }
+    setState(newState)
+  }
 
-  const onClick = (item) => {
-    console.log('Clicked price item', item);
-    formRef.current.setFormState(item)
+  const onSubmit= () => {
+    if (state.item === "" || state.price === 0 || state.price < 0) return;
+    state.id = Math.random().toString(36).substring(2, 6);
+    setState(NewItem);
+    priceItemSubmit(state)
   }
 
   return (
+    <tr>
+      <td><input type="text" name="item" value={state.item} className="form-input" onChange={onChange}  /></td>
+      <td>
+        <select className="custom-select mr-1" name="type" value={state.type} onChange={onChange}>
+          <option value="miscellaneous" defaultValue>Miscellaneous</option>
+          <option value="accommodation">Accommodation</option>
+          <option value="transportation">Transportation</option>
+          <option value="meals">Meals</option>
+          <option value="tourguide">Tourist guide fee</option>
+          <option value="entrance fee">Entrance fee</option>
+        </select>
+      </td>
+      <td><input type="number" name="price" className="text-right" value={state.price} onChange={onChange}  /></td>
+      <td>
+        <input
+          type="checkbox"
+          id='shared'
+          name="shared"
+          className="form-input"
+          checked={state.shared}
+          onChange={onChange}
+        />
+      </td>
+      <td>
+        <input
+          type="checkbox"
+          id='vat'
+          name="vat"
+          className="form-input"
+          checked={state.vat}
+          onChange={onChange}
+        />
+      </td>
+      <td>
+        <input
+          type="checkbox"
+          id='coh'
+          name="coh"
+          className="form-input"
+          checked={state.coh}
+          onChange={onChange}
+        />
+      </td>
+      <td>
+        <input 
+          type="text" 
+          name="remarks"
+          laceholder="remarks" 
+          className="form-input" 
+          value={state.remarks} 
+          onChange={onChange} 
+           
+        />
+      </td>
+      <td><FaCheckCircle style={{ color: 'green', fontSize: '1.5em' }} onClick={() => onSubmit()} /></td>
+    </tr>
+  )  
+}
+
+export default (props) => {
+  const { expenses } = useContext(PricingContext);
+  const { day } = props;
+  const todaysExpenses = expenses.filter(expense => expense.day === day);
+
+  return (
     <Wrapper>
-      { day > 1 && <CloseButton k={day} /> }
+      {day > 1 && <CloseButton k={day} />}
       <DayHeader>{`Day ${day}`}</DayHeader>
-      <Form k={day} ref={formRef} />
-      {todaysExpenses.length > 0 &&
         <ExpenseTable>
-          {todaysExpenses.map(item => <Expense key={item.id} item={item} onClick={() => onClick(item)} />)}
+          { todaysExpenses.length > 0 && todaysExpenses.map(item => <Expense key={item.id} item={item} />)}
+          <NewExpense day={day} />
         </ExpenseTable>
-      }
     </Wrapper>
   )
 }
